f4c5b12c20a7ed6ee097b0d1977ea2cf497aec56
madordie <keith_127@126.com>
2017-09-13 13:03:59 +0800

    demo

--- Demo-AppStore-More/ViewController.swift
+++ Demo-AppStore-More/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let text = MoreTextView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.white
+        view.addSubview(text)
+
+        text.frame = CGRect(x: 20, y: 20, width: 150, height: 10)
+        text.layer.borderWidth = 1
+        text.layer.borderColor = UIColor.gray.cgColor
+        text.isEditable = false
+        text.isScrollEnabled = false
+        text.textContainerInset = UIEdgeInsets(top: 0, left: -text.textContainer.lineFragmentPadding, bottom: 0, right: -text.textContainer.lineFragmentPadding)
+        text.font = UIFont.systemFont(ofSize: 12)
+        text.isSelectable = false
+        text.maxNumberOfLines = 3
+        text.more.font = UIFont.systemFont(ofSize: 12)
+
+        text.text = "这TMD都是啥都是啥都是啥都是啥都是啥都是啥都是啥都是啥都是啥都是啥都是啥都是啥都是啥都是啥。。。"
+        text.more.text = "查看更多"
+        text.more.textColor = UIColor.blue
+        text.more.adjustsFontSizeToFitWidth = true
+        text.more.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(showMore)))
+        text.more.isUserInteractionEnabled = true
+        text.sizeToFit()
+    }
+    @objc func showMore() {
+        if text.maxNumberOfLines != 0 {
+            text.maxNumberOfLines = 0
+            text.layoutSubviews()
+            text.sizeToFit()
+            print(text)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
